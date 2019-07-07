2bd06ce636caacc335694a75e51643afb4817b53
Hanna <anna.gokhberg@gmail.com>
2019-07-07 20:57:53 +0300

    initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: 'Orchid',
+    price: 799,
+    description: 'A low-maintenance flower. Needs to be watered twice a month'
+  },
+  {
+    name: 'Succulent',
+    price: 699,
+    description: 'A plant that stores water in leaves, stems, or both'
+  },
+  {
+    name: 'Lemon tree',
+    price: 299,
+    description: 'Beautiful and practical option for appartments with lots of space'
+  }
+];
