c49b8cd465a93d9f861d7faf471417d98e0134c9
Hanna <anna.gokhberg@gmail.com>
2019-07-10 12:25:51 +0300

    bugFix: main page buy button & picture onclick

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -9,6 +10,15 @@ import { products } from '../products';
 })
 export class ProductListComponent {
   products = products;
+
+  constructor(private cartService: CartService){
+
+  }
+
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
 
   share() {
     window.alert('The product has been shared!');
